772590d3b54f4a5cadab07731721a1058fdd3be1
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2025-01-30 10:30:35 -0300

    teste : busca de produto por nome

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -13,10 +13,41 @@ import static org.hamcrest.CoreMatchers.is;
 public class ProductControllerRA {
 
     private Long existingProductId, nonExistingProductId, dependentProductId;
+    private String productName;
 
     @BeforeEach
     public void setup() throws JSONException {
         baseURI = "http://localhost:8080";
+
+        productName = "Macbook";
+    }
+
+    @Test
+    public void findAllShouldReturnPagedProductsWhenProductNameParamIsEmpty() {
+        given()
+                .get("/products?page=0")
+                .then()
+                .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
+
+    @Test
+    public void findAllShouldReturnPagedProductsWhenProductNameParamIsNotEmpty() {
+        given()
+                .get("/products?name={productName}", productName)
+                .then()
+                .statusCode(200)
+                .body("content.id[0]", is(3))
+                .body("content.name[0]", equalTo("Macbook Pro"))
+                .body("content.price[0]", is(1250.0f))
+                .body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+    }
+
+    @Test
+    public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000() {
+        given()
+                .get("/products?size=25")
+                .then()
+                .body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
     }
 
     @Test
