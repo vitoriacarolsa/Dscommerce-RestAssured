936780ed1671382b90fbfd4021da59efce812a06
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2025-01-30 15:54:22 -0300

    inserindo com usuario sem permissao de acesso ou token invalido

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -221,6 +221,38 @@ public class ProductControllerRA {
                 .body("errors.message[0]", equalTo("Deve ter pelo menos uma categoria"));
     }
 
+    @Test
+    public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(newProduct)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+        JSONObject newProduct = new JSONObject(postProductInstance);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + invalidToken)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(newProduct)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(401);
+    }
+
 
 
 
