6bd20f7fb2e64856969ad53819fa1dbbff8518ce
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2025-01-30 10:14:41 -0300

    restassured

---
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ProductControllerRA {
+
+    private Long existingProductId, nonExistingProductId, dependentProductId;
+
+    @BeforeEach
+    public void setup() throws JSONException {
+        baseURI = "http://localhost:8080";
+    }
+
+    @Test
+    public void findByIdShouldReturnProductWhenIdExists() {
+        existingProductId = 2L;
+
+        given()
+                .get("/products/{id}", existingProductId)
+                .then()
+                .statusCode(200)
+                .body("id", is(2))
+                .body("name", equalTo("Smart TV"))
+                .body("imgUrl", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"))
+                .body("price", is(2190.0F))
+                .body("categories.id", hasItems(2, 3))
+                .body("categories.name", hasItems("Eletrônicos", "Computadores"));
+    }
+
+}
